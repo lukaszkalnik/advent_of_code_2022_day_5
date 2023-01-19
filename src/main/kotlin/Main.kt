38212343498f93a749c2f72736c562fdb74e4c81
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-01-19 21:51:23 +0100

    Parse table

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,24 +6,26 @@ fun main(args: Array<String>) {
     val path = args[0].toPath()
 
     FileSystem.SYSTEM.read(path) {
-       val stacks = readStacks()
+        val stacks = readStacks()
+        stacks.forEach { println(it) }
     }
 }
 
-private fun BufferedSource.readStacks(): List<ArrayDeque<Char>> {
+private fun BufferedSource.readStacks(): List<List<Char>> {
     val lines = mutableListOf<String>()
 
     while (true) {
         val line = readUtf8Line() ?: break
-        if (line.isEmpty()) return parseStacks(lines)
+        if (line.isEmpty()) return parseTable(lines)
 
         lines += line
     }
     throw IllegalStateException("EOF while reading crates")
 }
 
-private fun parseStacks(lines: List<String>): List<ArrayDeque<Char>> {
-    var stacks = emptyList<ArrayDeque<Char>>()
-
-    return stacks
+private const val CELL_WIDTH = 4
+private const val CHAR_POSITION = 1
+private fun parseTable(lines: List<String>): List<List<Char>> = lines.map { line ->
+    val cells = line.chunked(CELL_WIDTH)
+    cells.map { it.elementAt(CHAR_POSITION) }
 }
