796560b5914869496190acb0da5a4f407fe893da
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-01-23 20:02:28 +0100

    Parse move; add comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,15 @@ fun main(args: Array<String>) {
 
     FileSystem.SYSTEM.read(path) {
         val stacks = readStacks()
-        stacks.forEach { println(it) }
+
+        while (true) {
+            val line = readUtf8Line() ?: break
+            if (line.isEmpty()) break
+
+            val move = parseMove(line)
+            println(line)
+            println(move)
+        }
     }
 }
 
@@ -30,16 +38,34 @@ private fun parseStacks(lines: List<String>): List<ArrayDeque<Char>> {
 
     return List(stacksWidth) { stackNumber ->
         List(stackHeight) { cratePosition ->
-            table[cratePosition][stackNumber]
-        }.dropLastWhile { it == ' ' }
-            .let { ArrayDeque(it) }
+            table[cratePosition][stackNumber] // transpose the matrix to get list of vertical stacks
+        }.dropLastWhile { it == ' ' } // drop empty crate positions
+            .let { ArrayDeque(it) } // Kotlin equivalent of a stack
     }
 }
 
+// Table has constant cell width
 private const val CELL_WIDTH = 4
 private const val CHAR_POSITION = 1
 private fun parseTable(lines: List<String>): List<List<Char>> = lines.map { line ->
     line.chunked(CELL_WIDTH)
         .map { it.elementAt(CHAR_POSITION) }
-}.dropLast(1)
-    .reversed()
+}.dropLast(1) // in the input data the last line is just stack indexes
+    .reversed() // in the input data the start of the stack is at the bottom
+
+data class Move(
+    val number: Int,
+    val from: Int,
+    val to: Int,
+)
+
+private val MOVE_REGEX = "^move (\\d+) from (\\d) to (\\d)\$".toRegex()
+fun parseMove(line: String): Move {
+    val matchResult = MOVE_REGEX.find(line)!!
+    val (number, from, to) = matchResult.destructured
+    return Move(
+        number = number.toInt(),
+        from = from.toInt() - 1, // in the input data stacks are indexed starting from 1
+        to = to.toInt() - 1,
+    )
+}
